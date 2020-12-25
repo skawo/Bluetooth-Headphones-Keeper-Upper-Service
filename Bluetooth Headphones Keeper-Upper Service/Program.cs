91e61a01cc4c7726fa9f06aa785f299a51096a2e
skawo <skawo90@gmail.com>
2020-12-25 13:41:37 +0100

    Initial commit

---
+++ Bluetooth Headphones Keeper-Upper Service/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Topshelf;
+using System.Media;
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+using System.Threading;
+
+namespace Bluetooth_Headphones_Keeper_Upper_Service
+{
+    class Program
+    {
+        public class BluetoothAudioKeeperUpper
+        {
+            private SoundPlayer Player = null;
+            bool AbortThread = false;
+            Thread PlayerThread;
+
+            private MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
+            private NotificationClientImplementation NCI;
+            private IMMNotificationClient NC;
+
+            public BluetoothAudioKeeperUpper()
+            {
+                NCI = new NotificationClientImplementation();
+                NC = NCI;
+                deviceEnum.RegisterEndpointNotificationCallback(NC);
+
+                System.IO.Stream Sound = Properties.Res.Silence;
+                Player = new SoundPlayer(Sound);
+            }
+
+            private void Execute(Object obj)
+            {
+                while (!AbortThread)
+                {
+                    if (NCI.DoRestart)
+                    {
+                        Player.Stop();
+                        Player.PlayLooping();
+                        NCI.DoRestart = false;
+                    }
+
+                    Thread.Sleep(1000);
+                }
+
+                Player.Stop();
+            }
+
+            internal class NotificationClientImplementation : IMMNotificationClient
+            {
+                public bool DoRestart = true;
+                
+                public void OnDeviceAdded(string deviceId) { }
+
+                public void OnDeviceRemoved(string deviceId) { }
+
+                public NotificationClientImplementation() { }
+
+                public void OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key) { }
+
+                public void OnDeviceStateChanged(string deviceId, DeviceState newState) { DoRestart = true; }
+
+                public void OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId) { DoRestart = true; }
+            }
+
+            public void Start() { AbortThread = false; PlayerThread = new Thread(Execute); PlayerThread.Start(); }
+            public void Stop() { AbortThread = true; PlayerThread.Join(); }
+
+        }
+
+        public static void Main()
+        {
+            var rc = HostFactory.Run(x =>                                  
+            {
+                x.Service<BluetoothAudioKeeperUpper>(s =>                                   
+                {
+                    s.ConstructUsing(name => new BluetoothAudioKeeperUpper());                
+                    s.WhenStarted(tc => tc.Start());                        
+                    s.WhenStopped(tc => tc.Stop());                         
+                });
+                x.RunAsLocalSystem();                                      
+
+                x.SetDescription("Service which keeps Bluetooth headphones always on, to prevent the audio startup lag problem.");                  
+                x.SetDisplayName("Bluetooth Heaphones Keeper-Upper Service");                                 
+                x.SetServiceName("Bluetooth Heaphones Keeper-Upper Service");                                 
+            });                                                             
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Bluetooth-Headphones-Keeper-Upper-Service
+A service that prevents Bluetooth headphones from shutting off.
+
+Some of the cheaper Bluetooth headphones will cut off the beginning of the sound, if said sound happens after a bout of silence.
+Some may also have a hearable difference for sound being off and sound being on, which can be annoying.
+
+This prevents both of these issues by simply playing a silent looping sound in the background. It's a service, so it
+will start automatically whenever you turn on your computer.
+
+Note: This might lower your headphones' battery life.
+
+## Installation
+
+Simply unzip the archive somewhere and run "Install.bat". You will be asked for admin privileges.
+Files will be copied to /Program Files(x86)/BluetoothKeeperService and installed from there.
+
+To uninstall, simply run "Uninstall.bat".
